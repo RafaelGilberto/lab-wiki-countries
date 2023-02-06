9ce80c53949f560d0eb0bd569658fe6d3d239166
Rafael Gilberto Rezende Lima <rgrl516@gmail.com>
2023-02-06 00:11:46 -0300

    bugs corrections in country details

--- src/components/CountryDetails/index.js
+++ src/components/CountryDetails/index.js
@@ -4,15 +4,15 @@ import { useParams } from 'react-router-dom';
 export function CountryDetails(props) {
   const params = useParams();
   const info = props.countries.filter((currentElement) => {
-    return currentElement.alpha3Code === Number(params.id);
-  });
+    return currentElement.alpha3Code === params.id;
+  })[0];
 
   return (
     <>
       <div>{info.name.common}</div>
       <div>
         <p>Capital : {info.capital}</p>
-        <p>Area : {info.area}</p>
+        <p>Area : {info.area} km^2</p>
         {info.borders.map((currentBorder) => {
           <p>{currentBorder}</p>;
         })}
